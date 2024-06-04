ce95d6d0ded66af2f18b25a4c861a9e6ad01ff92
Peterr134 <peterr134@nycstudents.net>
2024-06-04 09:15:26 -0400

    Initial commit

---
+++ src/main/java/net/peterlogan/javadim/JavadimClient.java
@@ -0,0 +1,8 @@
+package net.peterlogan.javadim;
+import net.fabricmc.api.ClientModInitializer;
+public class JavadimClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/peterlogan/javadim/JavadimDataGenerator.java
@@ -0,0 +1,11 @@
+package net.peterlogan.javadim;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JavadimDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
